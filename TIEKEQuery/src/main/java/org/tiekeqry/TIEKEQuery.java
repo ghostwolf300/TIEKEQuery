75704c678e3abd361d120995769eb48fdb092057
ville.susi <ville.susi@WAUTVANWS476.wihuri.local>
2019-09-17 11:03:48 +0300

    Connects to TIEKE...

--- TIEKEQuery/src/main/java/org/tiekeqry/TIEKEQuery.java
+++ TIEKEQuery/src/main/java/org/tiekeqry/TIEKEQuery.java
@@ -1,10 +1,22 @@
 package org.tiekeqry;
 
+import org.tiekeqry.net.NetConnector;
+import org.tiekeqry.net.TIEKEConnector;
+
 public class TIEKEQuery {
-
+	
+	public TIEKEQuery() {
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		TIEKEQuery tieke=new TIEKEQuery();
+		tieke.test();
+	}
+	
+	public void test() {
+		TIEKEConnector connector=new NetConnector();
+		connector.fetchTIEKEData(null);
 	}
 
 }
